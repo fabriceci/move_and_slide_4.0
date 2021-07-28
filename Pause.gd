5273aa6216c8908b1e00c7efbd4ecde25e0e14e4
PouleyKetchoupp <pouleyketchoup@gmail.com>
2021-07-28 10:51:30 -0700

    Add utility to pause and step physics manually
    P : toggle pause
    O : step once when paused

---
+++ Pause.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var paused = false
+var step_once = false
+
+func _physics_process(_delta):
+	if Input.is_action_just_pressed('pause'):
+		paused = !paused
+		get_tree().paused = paused
+		step_once = false
+	
+	if paused:
+		if step_once:
+			get_tree().paused = true
+			step_once = false
+		elif Input.is_action_just_pressed('step'):
+			# step once
+			get_tree().paused = false
+			step_once = true
